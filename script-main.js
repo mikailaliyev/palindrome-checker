b9df51177223c0b52b8943c3c9b857264d86fdd8
mikailaliyev <mikail.aliyev@gmail.com>
2022-04-25 23:07:37 +0400

    added basic palindrome checker function

--- script-main.js
+++ script-main.js
@@ -1,18 +1,23 @@
 const input = document.getElementById("input");
 const button = document.getElementById("btn");
 
-button.addEventListener("click", () => {
-  if (input.value === "helleh") {
-    return alert("A palindrome!");
+//setting fake checker function
+const checkIt = () => {
+  if (input.value.split("").reverse().join("") === input.value) {
+    alert("A palindrome!");
+    return (input.value = "");
   }
   alert("Not one!");
+  return (input.value = "");
+};
+
+//Starting checker func either with web button or with Enter button
+button.addEventListener("click", () => {
+  checkIt();
 });
 
 document.addEventListener("keydown", (event) => {
   if (["Enter"].includes(event.key)) {
-    if (input.value === "helleh") {
-      return alert("A palindrome!");
-    }
-    alert("Not one!");
+    checkIt()
   }
 });
