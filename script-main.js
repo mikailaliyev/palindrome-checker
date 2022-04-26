e5f4b5c2d9c4e8bc40a60dd914771a6b3bc06105
mikailaliyev <mikail.aliyev@gmail.com>
2022-04-26 23:41:51 +0400

    added main logic

--- script-main.js
+++ script-main.js
@@ -1,14 +1,28 @@
-const input = document.getElementById("input");
+let input = document.getElementById("input");
 const button = document.getElementById("btn");
-
+let result = document.getElementById("result");
+console.log(input);
 //setting initial checker function
 const checkIt = () => {
-  if (input.value.split("").reverse().join("") === input.value) {
-    alert("A palindrome!");
-    return (input.value = "");
+  let newArr = [];
+  let value = input.value;
+  if (input.value.length < 2) {
+    return (result.textContent = "Not valid");
   }
-  alert("Not one!");
-  return (input.value = "");
+  value = value.split("");
+  if (value.reverse().join("") === input.value) {
+    return (result.textContent = "This is a real palindrome!");
+  }
+  for (let i = 0; i < value.length; i++) {
+    if (!newArr.includes(value[i])) {
+      newArr.push(value[i]);
+    } else {
+      newArr = newArr.filter((item) => item !== value[i]);
+    }
+  }
+  console.log(newArr);
+  return (result.textContent =
+    newArr.length < 2 ? "Not a palindrom now, but might be one!" : "Nope, it's not a palindrome");
 };
 
 //Starting checker func either with web button or with Enter button
@@ -18,6 +32,6 @@ button.addEventListener("click", () => {
 
 document.addEventListener("keydown", (event) => {
   if (["Enter"].includes(event.key)) {
-    checkIt()
+    checkIt();
   }
 });
